4fa8366b7948eba844e15167b42894f5417ad257
Ahmet Faruk Erdem <93256027+kilanli@users.noreply.github.com>
2024-12-24 13:13:28 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require("cors");
 const logger = require("morgan");
 const mainRoute = require("./routes/index.js");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 dotenv.config();
 
